4e197f213d120d7d14fcb77ef2929933af175a5b
luizpastana <lcsp84@gmail.com>
2022-03-24 10:01:40 -0300

    Requisito 01 - concluido. Implementa os testes referentes aos links da aplicacao

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa se a aplicação contém um conjunto fixo de links de navegação', () => {
+  it('Testa o link "Home"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkHome).toBeInTheDocument();
+    userEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    const homeTitle = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  it('Testa o link "About"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+    const aboutTitle = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('Testa o link "Favorite Pokemons"', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+    const favoriteTitle = screen.getByRole('heading', { name: /Favorite Pokémons/i });
+    expect(favoriteTitle).toBeInTheDocument();
+  });
+});
